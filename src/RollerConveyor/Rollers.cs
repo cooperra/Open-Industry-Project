24b9427469d04017b78a8a915e808b72b3f5ce15
Robbie Cooper <cooperra@users.noreply.github.com>
2024-06-24 06:46:28 -0400

    Attwmpt to fix duplication error for rollers

--- src/RollerConveyor/Rollers.cs
+++ src/RollerConveyor/Rollers.cs
@@ -10,6 +10,8 @@ public partial class Rollers : Node3D
 		
 	RollerConveyor owner;
 
+	int uneditableRollerCount;
+
     public void ChangeScale(float scale)
     {
         int roundedScale = Mathf.RoundToInt(scale / rollersDistance) + 1;
@@ -17,12 +19,14 @@ public partial class Rollers : Node3D
         int desiredRollerCount = roundedScale - 2;
 
         int difference = desiredRollerCount - rollerCount;
+        int uneditableRollersMissing = uneditableRollerCount - rollerCount;
 
         if (difference > 0) 
         {
             for (int i = 0; i < difference; i++)
             {
-                SpawnRoller();
+                bool persist = i >= uneditableRollersMissing;
+                SpawnRoller(persist);
             }
         }
         else if (difference < 0) 
@@ -39,6 +43,8 @@ public partial class Rollers : Node3D
 	{
 		owner = GetParent() as RollerConveyor;
 
+		uneditableRollerCount = GetUneditableRollerCount();
+
 		FixRollers();
 	}
 	
@@ -49,22 +55,37 @@ public partial class Rollers : Node3D
 			Scale = new Vector3(1 / owner.Scale.X, 1, 1);
 		}
 	}
-	
-	void SpawnRoller()
+
+		void SpawnRoller(bool persist = true)
 	{
 		if (GetParent() == null || owner == null) return;
 		Roller roller = rollerScene.Instantiate() as Roller;
         AddChild(roller, forceReadableName: true);
-		roller.Owner = owner.Owner;
+		roller.Owner = persist ? GetTree().GetEditedSceneRoot() : owner;
         roller.Position = new Vector3(rollersDistance * GetChildCount(), 0, 0);
 		roller.speed = owner.Speed;
 		roller.RotationDegrees = new Vector3(roller.RotationDegrees.X, owner.SkewAngle, roller.RotationDegrees.Z);
 		FixRollers();
     }
-	
+
 
 	void FixRollers()
 	{
 		((Roller)GetChild(0)).Position = new Vector3(rollersDistance, 0, 0);
+	}
+
+	int GetUneditableRollerCount()
+	{
+		int count = 0;
+		Node editedScene = GetTree().GetEditedSceneRoot();
+		foreach (Node child in GetChildren())
+		{
+			if (child.Owner == editedScene)
+			{
+				break;
+			}
+			count++;
+		}
+		return count;
 	}
 }
